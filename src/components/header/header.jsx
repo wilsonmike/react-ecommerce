d4188cb14a1ac89e9ce6a8619853e2cfa3bb2d11
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-20 09:12:02 -0400

    added shopping cart

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CartIcon from "../cart-icon/cart-icon";
 import "./header.styles.scss";
 
 const Header = () => {
@@ -16,6 +17,7 @@ const Header = () => {
           <Link to="/shop">Shop</Link>
         </li>
       </ul>
+      <CartIcon />
     </nav>
   );
 };
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./hero.styles.scss";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-body">
+        <p className="title">Hero title</p>
+        <p className="subtitle">Hero subtitle</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
