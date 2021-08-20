00f23aaec15b8bce111d20f5feb66cd2eac54463
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-20 16:53:58 -0400

    second collage better - current push

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CarouselHero from "../carousel-hero/carousel-hero";
 import MainAsset from "../../assets/mainasset.png";
+import Collage from "../../assets/collage.png";
 import "./hero.styles.scss";
 
 const Hero = () => {
@@ -9,7 +10,7 @@ const Hero = () => {
       <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div className="col-10 col-sm-8 col-lg-6">
           <img
-            src={MainAsset}
+            src={Collage}
             class="d-block mx-lg-auto img-fluid main-asset"
             alt="Families wearing shirts made by Hope35"
             loading="lazy"
@@ -27,9 +28,9 @@ const Hero = () => {
           <div className="d-grid gap-2 d-md-flex justify-content-md-start">
             <button
               type="button"
-              className="btn btn-primary btn-lg px-4 me-md-2"
+              className="btn btn-outline-primary btn-lg px-4 me-md-2"
             >
-              Primary
+              Get Started
             </button>
             <button
               type="button"
