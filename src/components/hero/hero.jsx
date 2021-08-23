60968c03b20d4093234e77bcf247ad50881d7cfc
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-23 12:50:43 -0400

    landing page structure

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -6,32 +6,34 @@ import "./hero.styles.scss";
 
 const Hero = () => {
   return (
-    <div className="container  col-xxl-8 px-4 py-5">
-      <div className="row flex-lg-column-reverse align-items-center g-5 py-5">
-        <div className="col-10 col-sm-8 col-lg-6">
-          <img
-            src={Collage}
-            class="d-block mx-lg-auto img-fluid main-asset"
-            alt="Families wearing shirts made by Hope35"
-            loading="lazy"
-          />
-        </div>
-        <div className="col-lg-6 align-items-center text-align-center">
-          <h1 className=" lh-1 mb-3 mt-5 main-tag">
-            Support from friends and family improves recovery
-          </h1>
-          <hr className="hr-row" />
-          <p className="sub-tag">
-            Hope35 creates custom t-shirts and a unique online platform to share
-            photos and words of encouragement.
-          </p>
-          <div className="d-grid gap-2 d-md-flex justify-content-center">
-            <button
-              type="button"
-              className="btn gradient-button gradient-button-3 btn-md  me-md-2"
-            >
-              <strong>Get Started</strong>
-            </button>
+    <div className="raster">
+      <div className="container  col-xxl-8 px-4 py-5">
+        <div className="row flex-lg-column-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <img
+              src={Collage}
+              class="d-block mx-lg-auto img-fluid main-asset"
+              alt="Families wearing shirts made by Hope35"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-lg-6 align-items-center text-align-center">
+            <h1 className=" lh-1 mb-3 mt-5 main-tag">
+              Support from friends and family improves recovery
+            </h1>
+            <hr className="hr-row" />
+            <p className="sub-tag">
+              Hope35 creates custom t-shirts and a unique online platform to
+              share photos and words of encouragement.
+            </p>
+            <div className="d-grid gap-2 d-md-flex justify-content-center">
+              <button
+                type="button"
+                className="btn gradient-button gradient-button-3 btn-md  me-md-2"
+              >
+                <strong>Get Started</strong>
+              </button>
+            </div>
           </div>
         </div>
       </div>
