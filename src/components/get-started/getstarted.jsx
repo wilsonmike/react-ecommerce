05732de8740885852e59ed877b7444ea6b3276d9
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-23 16:49:21 -0400

    working on page 2

--- src/components/get-started/getstarted.jsx
+++ src/components/get-started/getstarted.jsx
@@ -2,11 +2,21 @@ import "./getstarted.styles.scss";
 
 const GetStarted = () => {
   return (
-    <div className="main">
-      <div className="container">
-        <h6 className="tagged">
-          Open a store, share photos, or shop t-shirts.
-        </h6>
+    <div className="over-arch">
+      <div className="main">
+        <div className="container">
+          <h6 className="tagged">
+            Open a store, shop t-shirts, or share photos.
+          </h6>
+          <p>
+            <em>Choose your category</em>
+          </p>
+        </div>
+      </div>
+      <div className="hashed">
+        <div className="card">test</div>
+        <div className="card">test</div>
+        <div className="card">test</div>
       </div>
     </div>
   );
