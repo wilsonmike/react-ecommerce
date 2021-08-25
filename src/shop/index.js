14706494852724b072e53dc1cd5ad297c1991663
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-25 12:30:41 -0400

    added mock shop data

---
+++ src/shop/index.js
@@ -0,0 +1,60 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: "Blue Ashley Tee",
+    description: "Blue Ashley Tee, softest 50/50 blend.",
+    size: "Small",
+    price: 25,
+  },
+  {
+    id: 2,
+    title: "Blue Ashley Tee",
+    description: "Blue Ashley Tee, softest 50/50 blend.",
+    size: "Medium",
+    price: 25,
+  },
+  {
+    id: 3,
+    title: "Blue Ashley Tee",
+    description: "Blue Ashley Tee, softest 50/50 blend.",
+    size: "Large",
+    price: 25,
+  },
+  {
+    id: 4,
+    title: "Blue Ashley Tee",
+    description: "Blue Ashley Tee, softest 50/50 blend.",
+    size: "Extra Large",
+    price: 25,
+  },
+  {
+    id: 5,
+    title: "Pink Ashley Tee",
+    description: "Blue Ashley Tee, softest 50/50 blend.",
+    size: "Small",
+    price: 25,
+  },
+  {
+    id: 6,
+    title: "Pink Ashley Tee",
+    description: "Blue Ashley Tee, softest 50/50 blend.",
+    size: "Medium",
+    price: 25,
+  },
+  {
+    id: 7,
+    title: "Pink Ashley Tee",
+    description: "Blue Ashley Tee, softest 50/50 blend.",
+    size: "Large",
+    price: 25,
+  },
+  {
+    id: 8,
+    title: "Pink Ashley Tee",
+    description: "Blue Ashley Tee, softest 50/50 blend.",
+    size: "Extra Large",
+    price: 25,
+  },
+];
+
+export default SHOP_DATA;
