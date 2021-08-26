0fb09f3b5c40009082503fb4d183f97b43ccab95
mike <michaelwilsonsj@hotmail.com>
2021-08-26 17:15:33 -0400

    single product view working

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import "./App.scss";
 import { Switch, Route } from "react-router-dom";
 import GetStartedPage from "./components/get-started";
 import AshleyChasePage from "./components/ashley-chase-page";
+import SingleProduct from "./components/single-product/single-product";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/get-started" component={GetStartedPage} />
+      <Route path='/product/:id' component={SingleProduct} />
       <Route exact path="/ashleychase" component={AshleyChasePage} />
     </Switch>
   );
