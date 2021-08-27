cb7a448c2dded7583cf8094d9ab5c4d87b3ae3e0
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-27 15:53:15 -0400

    cleaned shop data - two garments && styling cart page

--- src/components/pages/cart-page/cart-items.jsx
+++ src/components/pages/cart-page/cart-items.jsx
@@ -9,27 +9,29 @@ const CartItem = (product) => {
       <div className="item-image">
         <img src={imageUrl} alt="Product" />
       </div>
-      <div className="name-price">
-        <h4>{title}</h4>
-        <p>${price}</p>
-      </div>
-      <div className="quantity">
-        <p>{`Quantity: ${quantity}`}</p>
-      </div>
-      <div className="btns-container">
-        <button className="btn-increase">
-          <PlusCircleIcon width="20px" />
-        </button>
-        {quantity === 1 && (
-          <button className="btn-trash">
-            <TrashIcon width="20px" />
+      <div className="details">
+        <div className="name-price">
+          <h4>{title}</h4>
+          <p>${price}</p>
+        </div>
+        <div className="quantity">
+          <p>{`Quantity: ${quantity}`}</p>
+        </div>
+        <div className="btns-container">
+          <button className="btn-increase">
+            <PlusCircleIcon width="20px" />
           </button>
-        )}
-        {quantity > 1 && (
-          <button className="btn-decrease">
-            <MinusCircleIcon width="20px" />
-          </button>
-        )}
+          {quantity === 1 && (
+            <button className="btn-trash">
+              <TrashIcon width="20px" />
+            </button>
+          )}
+          {quantity > 1 && (
+            <button className="btn-decrease">
+              <MinusCircleIcon width="20px" />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/shop/index.js
+++ src/shop/index.js
@@ -9,66 +9,12 @@ const SHOP_DATA = [
     price: 25,
   },
   {
-    id: 2,
-    title: "Blue Ashley Tee",
-    description: "Blue Ashley Tee, softest 50/50 blend.",
-    imageUrl:
-      "https://firebasestorage.googleapis.com/v0/b/hopeimage.appspot.com/o/blue-ashley.png?alt=media&token=b1899ff7-6220-4e12-9bb2-418089728f60",
-    size: "Medium",
-    price: 25,
-  },
-  {
-    id: 3,
-    title: "Blue Ashley Tee",
-    description: "Blue Ashley Tee, softest 50/50 blend.",
-    imageUrl:
-      "https://firebasestorage.googleapis.com/v0/b/hopeimage.appspot.com/o/blue-ashley.png?alt=media&token=b1899ff7-6220-4e12-9bb2-418089728f60",
-    size: "Large",
-    price: 25,
-  },
-  {
-    id: 4,
-    title: "Blue Ashley Tee",
-    description: "Blue Ashley Tee, softest 50/50 blend.",
-    imageUrl:
-      "https://firebasestorage.googleapis.com/v0/b/hopeimage.appspot.com/o/blue-ashley.png?alt=media&token=b1899ff7-6220-4e12-9bb2-418089728f60",
-    size: "Extra Large",
-    price: 25,
-  },
-  {
     id: 5,
     title: "Pink Ashley Tee",
     description: "Blue Ashley Tee, softest 50/50 blend.",
     imageUrl:
       "https://firebasestorage.googleapis.com/v0/b/hopeimage.appspot.com/o/pink-ashley.png?alt=media&token=fa5e8beb-1817-4c02-8978-0f5f128cd019",
     size: "Small",
-    price: 25,
-  },
-  {
-    id: 6,
-    title: "Pink Ashley Tee",
-    description: "Blue Ashley Tee, softest 50/50 blend.",
-    imageUrl:
-      "https://firebasestorage.googleapis.com/v0/b/hopeimage.appspot.com/o/pink-ashley.png?alt=media&token=fa5e8beb-1817-4c02-8978-0f5f128cd019",
-    size: "Medium",
-    price: 25,
-  },
-  {
-    id: 7,
-    title: "Pink Ashley Tee",
-    description: "Blue Ashley Tee, softest 50/50 blend.",
-    imageUrl:
-      "https://firebasestorage.googleapis.com/v0/b/hopeimage.appspot.com/o/pink-ashley.png?alt=media&token=fa5e8beb-1817-4c02-8978-0f5f128cd019",
-    size: "Large",
-    price: 25,
-  },
-  {
-    id: 8,
-    title: "Pink Ashley Tee",
-    description: "Blue Ashley Tee, softest 50/50 blend.",
-    imageUrl:
-      "https://firebasestorage.googleapis.com/v0/b/hopeimage.appspot.com/o/pink-ashley.png?alt=media&token=fa5e8beb-1817-4c02-8978-0f5f128cd019",
-    size: "Extra Large",
     price: 25,
   },
 ];
