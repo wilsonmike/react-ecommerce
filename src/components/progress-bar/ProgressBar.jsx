388be68a239920d81033647156e34e27c12c7be0
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-31 10:51:31 -0400

    fixed firebase permissions

--- src/components/progress-bar/ProgressBar.jsx
+++ src/components/progress-bar/ProgressBar.jsx
@@ -4,6 +4,7 @@ import useStorage from "../../hooks/useStorage";
 
 const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
+  console.log(progress, url);
   return <div className="progress-bar">progress</div>;
 };
 
