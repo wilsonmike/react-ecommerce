1fedae7ec8f5744f53117347b7c44636bd732faf
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-31 09:23:41 -0400

    checking upload file type - through error on non png/jpeg

--- src/components/upload-form/UploadForm.jsx
+++ src/components/upload-form/UploadForm.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ["image/png", "image/jpeg"];
+
   const changeHandler = (e) => {
-    console.log("changed");
+    let selected = e.target.files[0];
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError("");
+    } else {
+      setFile(null);
+      setError("Please select an image file (png or jpeg)");
+    }
   };
   return (
     <form action="">
       <input type="file" onChange={changeHandler} />
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+      </div>
     </form>
   );
 };
