a168071592c94af3fbb5bd50bdd0041cb85e9cb3
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-31 14:51:29 -0400

    img hover opacity

--- src/components/image-grid/ImageGrid.jsx
+++ src/components/image-grid/ImageGrid.jsx
@@ -1,4 +1,5 @@
 import useFirestore from "../../hooks/useFirestore";
+import { motion } from "framer-motion";
 import "./image-grid.styles.scss";
 
 const ImageGrid = ({ setSelectedImg }) => {
@@ -8,13 +9,14 @@ const ImageGrid = ({ setSelectedImg }) => {
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div
+          <motion.div
             className="img-wrap"
             key={doc.id}
+            whileHover={{ opacity: 0.8 }}
             onClick={() => setSelectedImg(doc.url)}
           >
             <img src={doc.url} alt="Ashley t-shirt" />
-          </div>
+          </motion.div>
         ))}
     </div>
   );
