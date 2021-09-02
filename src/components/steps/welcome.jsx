416cdc656f40bba4e76c5fcdb2c364d1f38cbb45
wilsonmike <michaelwilsonsj@hotmail.com>
2021-09-02 12:06:40 -0400

    submit form

---
+++ src/components/steps/welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h4>Fill out this quick form and let us do the rest...</h4>
+    </div>
+  );
+};
+
+export default Welcome;
